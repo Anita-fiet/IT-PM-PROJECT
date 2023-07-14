380ef04b0c0c1ddfafab40620ff8a9ae797c14c9
benvicky <benvicky2018@gmail.com>
2023-07-14 17:07:41 +0200

    hospital.sql

---
+++ nextgen/database/hospital.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 12, 2023 at 02:40 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `fname` varchar(100) NOT NULL,
+  `last_appointment` date NOT NULL,
+  `time_one` varchar(100) NOT NULL,
+  `next_appointment` date NOT NULL,
+  `time_two` varchar(100) NOT NULL,
+  `treatment_planning` varchar(500) NOT NULL,
+  `progress_monitoring` varchar(500) NOT NULL,
+  PRIMARY KEY (`treatment_planning`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `contact` varchar(12) NOT NULL,
+  PRIMARY KEY (`contact`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recording`
+--
+
+CREATE TABLE IF NOT EXISTS `recording` (
+  `fname` varchar(100) NOT NULL,
+  `allergies` varchar(100) NOT NULL,
+  `treated` varchar(100) NOT NULL,
+  `under_treatment` varchar(100) NOT NULL,
+  `persistent` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`treated`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recording`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(22) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', '123'),
+(2, 'ssaka', '12');
